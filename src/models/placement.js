972c72b835eea2f686e006b507682c363b565d8b
Urchmaney <kingsobino@gmail.com>
2020-10-29 14:40:20 +0000

    fix lint error

--- src/models/placement.js
+++ src/models/placement.js
@@ -1,9 +1,9 @@
 const { Schema, model, Types } = require('mongoose');
 
 const Placement = new Schema({
-  company: { type: Types.ObjectId, required: true, ref: 'companies'},
+  company: { type: Types.ObjectId, required: true, ref: 'companies' },
   year: { type: Number, required: true },
-  students: [{ type:Types.ObjectId }],
+  students: [{ type: Types.ObjectId }],
 });
 
 module.exports = model('placements', Placement);
